be4f353020cb5ceb1c14e6e18ca588ccf0793717
oblogin <oblogin.ilya@gmail.com>
2022-04-30 15:29:53 +0300

    Add Gameplay Ability System To project

--- Source/Hopper/Hopper.Build.cs
+++ Source/Hopper/Hopper.Build.cs
@@ -8,6 +8,20 @@ public class Hopper : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"HeadMountedDisplay"
+		});
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks"
+		});
 	}
 }
